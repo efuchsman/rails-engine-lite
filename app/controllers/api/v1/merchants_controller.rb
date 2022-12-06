b4f89541ae6e98493fc13e8f67b656683b7227ec
Eli Fuchsman <efuchsman@gmail.com>
2022-12-05 18:05:56 -0700

    Complete merchant/:id

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,11 +1,22 @@
 class Api::V1::MerchantsController < ApplicationController
   before_action :all_merchants,  only: [:index]
-
+  before_action :one_merchant, only: [:show]
   def index
     render json: MerchantSerializer.new(@merchants)
   end
 
+  def show
+    render json: MerchantSerializer.new(@merchant)
+    if params[:id].nil?
+
+    end
+  end
+
   def all_merchants
     @merchants = Merchant.all
+  end
+
+  def one_merchant
+    @merchant = Merchant.find(params[:id])
   end
 end
--- spec/requests/api/v1/merchants/merchants_get_request_spec.rb
+++ spec/requests/api/v1/merchants/merchants_get_request_spec.rb
@@ -33,4 +33,56 @@ describe "Merchants API [GET] requests" do
       end
     end
   end
+
+  describe "GET /merchants/:id" do
+    before :each do
+      @merchants = create_list(:merchant, 3)
+      @merchant1 = @merchants.first
+    end
+
+    describe "when records exist" do
+      it 'returns a successful response' do
+        get "/api/v1/merchants/#{@merchant1.id}"
+
+        JSON.parse(response.body, symbolize_names: true)
+
+        expect(response).to be_successful
+      end
+
+      it 'returns a status code 200' do
+        get "/api/v1/merchants/#{@merchant1.id}"
+
+        JSON.parse(response.body, symbolize_names: true)
+
+        expect(response).to have_http_status(200)
+      end
+
+      it 'returns a merchant' do
+        get "/api/v1/merchants/#{@merchant1.id}"
+        merchant1 = JSON.parse(response.body, symbolize_names: true)
+
+        expect(merchant1.class).to be Hash
+        expect(merchant1).to have_key(:data)
+        expect(merchant1[:data].class).to be Hash
+        expect(merchant1[:data]).to have_key(:id)
+        expect(merchant1[:data]).to have_key(:type)
+        expect(merchant1[:data]).to have_key(:attributes)
+        expect(merchant1[:data][:attributes]).to have_key(:name)
+      end
+    end
+
+    describe 'When the record DNE' do
+      it 'returns status code 404' do
+        get "/api/v1/merchants/4"
+
+        expect(response).to have_http_status(404)
+      end
+
+      it 'returns a not found message' do
+        get "/api/v1/merchants/4"
+
+        expect(response.body).to match(/Couldn't find Merchant/)
+      end
+    end
+  end
 end
