181e09403912227e5210296bb6f2044bfd5b2fe2
Eli Fuchsman <efuchsman@gmail.com>
2022-12-06 11:01:26 -0700

    get /items

---
+++ spec/requests/api/v1/items/items_get_request_spec.rb
@@ -0,0 +1,55 @@
+require 'rails_helper'
+
+describe "Items API [GET]" do
+  describe "GET /items" do
+    describe "When the records exist" do
+      before :each do
+        @merchant = create(:merchant)
+        @item1 = Item.create!(name: "name1", description: "desc1", unit_price: 75000, merchant_id: @merchant.id)
+        @item2 = Item.create!(name: "name2", description: "desc2", unit_price: 85000, merchant_id: @merchant.id)
+      end
+
+
+      it "returns a successful response" do
+        get '/api/v1/items'
+
+        items = JSON.parse(response.body, symbolize_names: true)
+
+        expect(response).to be_successful
+      end
+
+      it 'returns a status code 200' do
+        get '/api/v1/items'
+
+        items = JSON.parse(response.body, symbolize_names: true)
+
+        expect(response).to have_http_status(200)
+      end
+
+      it 'returns all items' do
+        get '/api/v1/items'
+
+        items = JSON.parse(response.body, symbolize_names: true)
+
+        expect(items.class).to be Hash
+        expect(items).to have_key(:data)
+        expect(items[:data].count).to eq(2)
+        expect(items[:data].first).to have_key(:id)
+        expect(items[:data].first).to have_key(:type)
+        expect(items[:data].first).to have_key(:attributes)
+        expect(items[:data].class).to be Array
+        expect(items[:data].first.class).to be Hash
+
+        expect(items[:data].first[:attributes]).to have_key(:name)
+        expect(items[:data].first[:attributes]).to have_key(:description)
+        expect(items[:data].first[:attributes]).to have_key(:unit_price)
+        expect(items[:data].first[:attributes]).to have_key(:merchant_id)
+
+        expect(items[:data][0][:attributes][:name].class).to be String
+        expect(items[:data][0][:attributes][:description].class).to be String
+        expect(items[:data][0][:attributes][:unit_price].class).to be Float
+        expect(items[:data][0][:attributes][:merchant_id].class).to be Integer
+      end
+    end
+  end
+end
