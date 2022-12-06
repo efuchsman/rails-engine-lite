301783a2be7bf41d40c950cf2febbaa412286f2d
Eli Fuchsman <efuchsman@gmail.com>
2022-12-05 18:38:39 -0700

    Fix model classes for merchant and item

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -9,6 +9,8 @@ RSpec.describe Item, type: :model do
     it { should validate_presence_of :name }
     it { should validate_presence_of :description }
     it { should validate_presence_of :unit_price }
+    it { should validate_numericality_of(:unit_price).is_greater_than(0) }
     it { should validate_presence_of :merchant_id }
+    it { should validate_numericality_of(:merchant_id).is_greater_than(0) }
   end
 end
