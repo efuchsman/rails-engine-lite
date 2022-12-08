bcd489e8dfb63bcb2e7f86262bce467a9b0e50e8
Eli Fuchsman <efuchsman@gmail.com>
2022-12-08 09:13:43 -0700

    WIP Refactor item search controller

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -5,7 +5,7 @@ module Api
     module Items
       class SearchController < ApplicationController
         def index
-          if params[:name].present? && !params[:max_price].present? && !params[:min_price].present?
+          if self.name_search?
             items = Item.find_items_by_name(params[:name])
             # binding.pry
             if items.nil? || items.empty?
@@ -15,7 +15,7 @@ module Api
               render json: ItemSerializer.new(items)
             end
 
-          elsif params[:min_price].present? && !params[:name].present? && !params[:max_price].present? && params[:min_price].to_f.positive?
+          elsif self.positive_min_price_search?
             # binding.pry
             items = Item.min_price(params[:min_price])
             if items.nil?
@@ -24,10 +24,10 @@ module Api
               render json: ItemSerializer.new(items)
             end
 
-          elsif params[:min_price].present? && !params[:name].present? && !params[:max_price].present? && params[:min_price].to_f.negative?
+          elsif self.negative_min_price_search?
             render json: { errors: {} }, status: 400
 
-          elsif params[:max_price].present? && !params[:name].present? && !params[:min_price].present? && params[:max_price].to_f.positive?
+          elsif self.positive_max_price_search?
             items = Item.max_price(params[:max_price])
             if items.nil?
               render json: { data: {} }, status: 400
@@ -35,7 +35,7 @@ module Api
               render json: ItemSerializer.new(items)
             end
 
-          elsif !params[:min_price].present? && !params[:name].present? && params[:max_price].present? && params[:max_price].to_f.negative?
+          elsif self.negative_max_price_search?
             render json: { errors: {} }, status: 400
 
           else
@@ -55,6 +55,28 @@ module Api
         #     render json: ItemSerializer.new(item)
         #   end
         # end
+
+        private
+
+        def name_search?
+          params[:name].present? && !params[:max_price].present? && !params[:min_price].present?
+        end
+
+        def positive_min_price_search?
+          params[:min_price].present? && !params[:name].present? && !params[:max_price].present? && params[:min_price].to_f.positive?
+        end
+
+        def negative_min_price_search?
+          params[:min_price].present? && !params[:name].present? && !params[:max_price].present? && params[:min_price].to_f.negative?
+        end
+
+        def positive_max_price_search?
+          params[:max_price].present? && !params[:name].present? && !params[:min_price].present? && params[:max_price].to_f.positive?
+        end
+
+        def negative_max_price_search?
+          !params[:min_price].present? && !params[:name].present? && params[:max_price].present? && params[:max_price].to_f.negative?
+        end
       end
     end
   end
