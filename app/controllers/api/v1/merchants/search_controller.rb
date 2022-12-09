827bbd3553c96d6b5dcc3f2dbaecd70b58528e6f
Eli Fuchsman <104859844+efuchsman@users.noreply.github.com>
2022-12-08 22:04:47 -0700

    Revert "Revert "Feat rle part 2/eli""

---
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    module Merchants
+      class SearchController < ApplicationController
+
+        def index
+          if params[:name].present?
+            merchants = Merchant.find_all_merchants_by_name(params[:name])
+            if merchants.nil? || merchants.blank?
+              render json: { data: [] }, status: 400
+            else
+              render json: MerchantSerializer.new(merchants)
+            end
+          else
+            render json: { data: 'Invalid Search' }, status: 400
+          end
+        end
+
+        def show
+          if params[:name].present?
+            merchant = Merchant.find_a_merchant_by_name(params[:name])
+            if merchant.nil?
+              render json: { data: {} }, status: 400
+            else
+              render json: MerchantSerializer.new(merchant)
+            end
+          else
+            render json: { data: 'Invalid Search' }, status: 400
+          end
+        end
+      end
+    end
+  end
+end
