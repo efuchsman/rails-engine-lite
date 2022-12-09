63cf3412d8e9d1816b09e2811b22ce83df8d311d
Eli Fuchsman <efuchsman@gmail.com>
2022-12-08 19:08:13 -0700

    WIP continued refactored

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -5,60 +5,86 @@ module Api
     class ItemsController < ApplicationController
       def index
         if params[:merchant_id]
-          @merchant = Merchant.find(params[:merchant_id])
-          @items = @merchant.items
-
-          render json: ItemSerializer.new(@items)
+          find_merchant
+          render_serialized_json(merchant_items)
         else
-          render json: ItemSerializer.new(Item.all)
+          render_serialized_json(find_items)
         end
       end
 
       def show
         if params[:merchant_id]
-          @merchant = Merchant.find(params[:merchant_id])
-          @item = @merchant.items.find(params[:id])
-
-          render json: ItemSerializer.new(@item)
+          find_merchant
+          render_serialized_json(merchant_item)
         else
-          render json: ItemSerializer.new(Item.find(params[:id]))
+          render_serialized_json(find_item)
         end
       end
 
       def create
         item = Item.new(item_params)
-
         if item.save
-          render json: ItemSerializer.new(item), status: :created
-
+          render_created_serialized_json(item)
         else
-          render json: {
-                   errors: item.errors,
-                   error_codes: item.errors.keys.map { |f| "#{f.upcase}_ERROR" }
-                 },
-                 status: 422
+          error_map(item)
         end
       end
 
       def update
-        @item = Item.find(params[:id])
-        if @item.update(item_params)
-          render json: ItemSerializer.new(@item), status: 202
+        find_item
+        if find_item.update(item_params)
+          render json: ItemSerializer.new(find_item), status: 202
         else
           render status: 404
         end
       end
 
       def destroy
-        item = Item.find(params[:id])
-        item.invoices.delete_invoices
-        render json: item.destroy
+        find_item
+        render json: find_item.destroy
+        Invoice.delete_invoices
       end
 
       private
 
       def item_params
         params.require(:item).permit(:name, :description, :unit_price, :merchant_id)
+      end
+
+      def find_merchant
+        @merchant = Merchant.find(params[:merchant_id])
+      end
+
+      def merchant_items
+        @merchant.items
+      end
+
+      def merchant_item
+        merchant_items.find(params[:id])
+      end
+
+      def find_items
+        Item.all
+      end
+
+      def find_item
+        Item.find(params[:id])
+      end
+
+      def render_serialized_json(arg)
+        render json: ItemSerializer.new(arg)
+      end
+
+      def render_created_serialized_json(arg)
+        render json: ItemSerializer.new(arg), status: :created
+      end
+
+      def error_map(arg)
+        render json: {
+          errors: arg.errors,
+          error_codes: arg.errors.keys.map { |f| "#{f.upcase}_ERROR" }
+        },
+        status: 422
       end
     end
   end
